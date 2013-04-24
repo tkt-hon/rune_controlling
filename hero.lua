b70b6debef235ffab5d29bb37d96fc678d05dbd3
Sami Saada <saada@cs.helsinki.fi>
2013-04-24 21:04:16 +0300

    just what hero needs

--- hero.lua
+++ hero.lua
@@ -2,35 +2,19 @@ local _G = getfenv(0)
 
 local M = {}
 
-local traveling = false
-
-local function IsRuneUp(teambot)
-  return teambot.data.rune and teambot.data.rune:CanPickRune()
+local function CanPickRune(teambot)
+  return teambot.data and teambot.data.rune and teambot.data.rune:CanPickRune()
 end
-M.IsRuneUp = IsRuneUp
+M.CanPickRune = CanPickRune
 
-local function PickRune(bot, unit, teambot, action, distance)
-  action = action or function(rune)
-    bot:OrderEntity(unit, "Touch", rune)
-  end
-  distance = distance or 100
-  local runeData = teambot.data.rune
-  local runeLocation = runeData:GetRuneLocation()
-  if traveling then
-    local beha = unit:GetBehavior()
-    if beha and Vector3.Distance2D(beha:GetGoalPosition(), runeLocation) > 200 then
-      bot:OrderPosition(unit, "Move", runeLocation)
-      return
-    end
-    if Vector3.Distance2D(unit:GetPosition(), runeLocation) <= distance then
-      action(runeData:GetRuneEntity())
-      traveling = false
-    end
-  else
-    bot:OrderPosition(unit, "Move", runeLocation)
-    traveling = true
-  end
+local function GetRuneLocation(teambot)
+  return teambot.data and teambot.data.rune and teambot.data.rune:GetRuneLocation()
 end
-M.PickRune = PickRune
+M.GetRuneLocation = GetRuneLocation
+
+local function GetRuneEntity(teambot)
+  return teambot.data and teambot.data.rune and teambot.data.rune:GetRuneEntity()
+end
+M.GetRuneEntity = GetRuneEntity
 
 Utils_RuneControlling_Hero = M
